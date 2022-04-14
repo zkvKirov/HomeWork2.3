1271ed473794dc67d9b62127c016cea0a9533a24
zkvKirov <zkkirov@yandex.ru>
2022-04-14 20:15:33 +0300

    little changes in Attachment

--- src/main/kotlin/ru/netology/Attachment.kt
+++ src/main/kotlin/ru/netology/Attachment.kt
@@ -2,33 +2,30 @@ package ru.netology
 
 sealed class Attachment {
 
+    abstract val type: String
+
     data class AudioAttachment (
         override val type: String = "audio",
-        val audio: Audio = Audio(234, 56756, "Кино", "Группа крови", 85,
-            "url", 345, 678, 1991)
+        val audio: Audio
     ) : Attachment()
 
     data class VideoAttachment (
         override val type: String = "video",
-        val video: Video = Video(123, 456, "Война", "Кино про войну", 90, null,
-            1998, 6042022, 55, 0, false, true, 720, 436)
+        val video: Video
     ) : Attachment()
 
     data class PhotoAttachment (
         override val type: String = "photo",
-        val photo: Photo = Photo(222, 333, "Nature", 444,
-            "какой-то текст", 6042022, null, 400, 300)
+        val photo: Photo
     ) : Attachment ()
 
     data class GraffitiAttachment (
         override val type: String = "graffiti",
-        val graffiti: Graffiti = Graffiti(777, 999, "url", 720, 540)
+        val graffiti: Graffiti
     ) : Attachment ()
 
     data class FileAttachment (
         override val type: String = "file",
-        val file: File = File (111, "Документ", 222, 540, "ext", "url", 6042022, 0)
+        val file: File
     ) : Attachment ()
-
-    abstract val type: String
 }
